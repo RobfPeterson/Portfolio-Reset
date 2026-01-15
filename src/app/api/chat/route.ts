86ceae25781e028bb315ea7e9666a02948d40b4e
Robert F. Peterson <rfpeterson25@gmail.com>
2026-01-15 12:40:26 -0600

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -109,7 +109,7 @@ export async function POST(req: Request) {
       retriever: historyAwareRetrievalChain, // get the relevant documents based on chat history
     });
 
-    await retrievalChain.invoke({
+    await retrievalChain.stream({
       input: latestMessage,
       chat_history: chatHistory,
     });
