2ece44e40e2b39b4c4108bb75af3c9b31e2bc5e6
Robert F. Peterson <rfpeterson25@gmail.com>
2026-01-20 16:59:10 -0600

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,14 +1,14 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs";
 import careerData from "@/data/career.json";
 import educationData from "@/data/education.json";
-import activityData from "@/data/activities.json";
-import { careerSchema, educationSchema ,activitySchema} from "@/lib/schemas";
+import activitiesData from "@/data/activities.json";
+import { careerSchema, educationSchema ,activitiesSchema} from "@/lib/schemas";
 import Timeline from "./Timeline";
 
 export default function Experience() {
   const career = careerSchema.parse(careerData).career;
   const education = educationSchema.parse(educationData).education;
-  const activities = activitySchema.parse(activityData).activities;
+  const activities = activitiesSchema.parse(activitiesData).activities;
   return (
     <Tabs defaultValue="work">
       <TabsList className="mb-2 grid w-full grid-cols-3">
@@ -23,7 +23,7 @@ export default function Experience() {
         <Timeline experience={education}></Timeline>
       </TabsContent>
        <TabsContent value="activities">
-        <Timeline activity={activitives}></Timeline>
+        <Timeline activities={activitives}></Timeline>
       </TabsContent>
     </Tabs>
   );
